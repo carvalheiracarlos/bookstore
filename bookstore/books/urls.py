57c55097ed4373cdc3f25dffa5ab9598f60c5a0a
carvalheirafc <carvalheirafc@gmail.com>
2024-06-21 18:53:00 -0300

    fixed routes

--- bookstore/books/urls.py
+++ bookstore/books/urls.py
@@ -4,7 +4,9 @@ from books.views import BookViewSet, BookAuthorViewSet, BookCategoryViewSet
 
 
 router = routers.DefaultRouter()
+router.register(r'book', BookViewSet, basename='BookViewSet')
 router.register(r'authors', BookAuthorViewSet, basename='BookAuthorViewSet')
+router.register(r'category', BookCategoryViewSet, basename='BookCategoryViewSet')
 
 app_name = "books"
 urlpatterns = []
