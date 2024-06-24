ad24334ac43b90551f37f1fe85bcce1036d01a8b
carvalheirafc <carvalheirafc@gmail.com>
2024-06-23 22:06:50 -0300

    made swagger root url path

--- bookstore/bookstore/urls.py
+++ bookstore/bookstore/urls.py
@@ -23,8 +23,8 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
